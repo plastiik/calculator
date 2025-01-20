5f280fe2a090f110edfb94ec7b5046eb9dc71572
Bruno Mota <bmota001@gmail.com>
2025-01-20 13:31:22 +0000

    Adds routes and view for calculator

---
+++ src/Controller/Calculator.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bridge\Twig\Attribute\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Attribute\Route;
+
+class Calculator extends AbstractController
+{
+    #[Route('/calculator', name: 'view')]
+    #[Template('calculator.html.twig')]
+    public function view(): array
+    {
+        return ['test' => 'test'];
+    }
+}
