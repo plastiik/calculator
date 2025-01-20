ef74969190af4cecdf3110d48fc42d31abe960e8
Bruno Mota <bmota001@gmail.com>
2025-01-20 12:15:13 +0000

    Adds composer finish job to pipeline

--- frankenphp/docker-entrypoint.sh
+++ frankenphp/docker-entrypoint.sh
@@ -24,7 +24,6 @@ if [ "$1" = 'frankenphp' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 
 	if [ -z "$(ls -A 'vendor/' 2>/dev/null)" ]; then
 		composer install --prefer-dist --no-progress --no-interaction
-		composer finish
 	fi
 
 	# Display information about the current project
