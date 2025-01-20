2136223712466feee518c37c0c6e81b754b50624
Bruno Mota <bmota001@gmail.com>
2025-01-20 12:10:51 +0000

    Analyse code

--- frankenphp/docker-entrypoint.sh
+++ frankenphp/docker-entrypoint.sh
@@ -24,6 +24,7 @@ if [ "$1" = 'frankenphp' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 
 	if [ -z "$(ls -A 'vendor/' 2>/dev/null)" ]; then
 		composer install --prefer-dist --no-progress --no-interaction
+		composer finish
 	fi
 
 	# Display information about the current project
